b1a607e43d50551af353d2ddd2b199c6d8be338c
Dalimil <dalimilhajek@gmail.com>
2016-01-30 16:17:17 +0000

    Add register page

--- main.py
+++ main.py
@@ -2,19 +2,19 @@ from flask import Flask
 from flask import render_template, request, redirect, session, url_for, escape, make_response, flash, abort
 
 app = Flask(__name__)
-app.secret_key = "bnNoqxXSgzoXSZjb8mrMp5L0L4mJ4o8nRzn"
+app.secret_key = "bnNoqxXSgzfdajkdsafadsoXSZjb8mrMp5L0L4mJ4o8nRzn"
 
 @app.route('/')
 def index():
 	return render_template('index.html')
-	
+
 @app.route('/authenticate')
 def authenticate():
 	return "authenticate here"
 
 @app.route('/register')
 def register():
-	return "register here"
+	return render_template('register.html')
 
 @app.route('/error')
 def error():
