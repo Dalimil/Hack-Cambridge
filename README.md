d1fc5fd1befed0eef429d84261eda59ecfbfbc96
Dalimil <dalimilhajek@gmail.com>
2016-01-30 15:59:48 +0000

    Basic Flask init

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Hack-Cambridge
 Hack Cambridge hackathon
+
+
+## Installation
+
+#### Install required packages
+```sh
+pip install -r requirements.txt
+```
+
+#### Start the server (localhost:8080):
+```sh
+python main.py
+```
---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask import render_template, request, redirect, session, url_for, escape, make_response, flash, abort
+import database
+
+app = Flask(__name__)
+app.secret_key = "bnNoqxXSgzoXSZjb8mrMp5L0L4mJ4o8nRzn"
+
+@app.route('/authenticate')
+def authenticate():
+	return "authenticate here"
+
+@app.route('/register')
+def register():
+	return "register here"
+
+@app.route('/error')
+def error():
+	abort(401)
+
+
+if __name__ == '__main__':
+	#host='0.0.0.0' only with debug disabled
+	app.run(port=8080, debug=True) 
